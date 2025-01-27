1534e2e7b84c1a101587eb7d7beb74bb12d02bb0
PabloRR100 <pabloruizruiz10@gmail.com>
2025-01-27 08:50:51 +0100

    Finished module 2

---
+++ hello_world/hello_world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello_world
+//
+//  Created by Pablo Ruiz Ruiz on 27/1/25.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
